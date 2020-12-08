534f10280c5d3cf3883126e18563409567f97936
ranielleru <rer141@scarletmail.rutgers.edu>
2020-12-07 20:49:20 -0500

    Python Project Milestone 2
    added reactive responses to each prompt

--- python_project.py
+++ python_project.py
@@ -3,16 +3,19 @@ def question_one():
     print "How well do you know yourself?"
 # Naturally, I want the user to provide their response in a fill-in box
     fill_in = raw_input("Enter your answer: ")
+    print "Response archived!"
 question_one()
 
 # I originally wrote the next two questions under the first function but it didn't seem to work so I separated them
 def question_two():
     print "What are at least four or five things you are definitely certain about yourself?"
     second_fill_in = raw_input("Enter your answer: ")
+    print "Response archived! Think about the things you're uncertain about yourself and explore those parts more!"
 question_two()
 
 # The third question is supposed to somewhat relate to the second and first question and wraps up the prompts for the day
 def question_three():
     print "What are at least two or three traits you used to exhibit that you no longer do?"
     third_fill_in = raw_input("Enter your answer: ")
+    print "Response archived! Be proud of how far you've come and how much growth is ahead of you!"
 question_three()
